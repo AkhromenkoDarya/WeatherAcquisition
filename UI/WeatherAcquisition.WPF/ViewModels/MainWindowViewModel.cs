046b6461209c1a558e11e4d71985fb6f8be2400e
Darya Akhromenko <otulissa2012@yandex.ru>
2022-07-18 15:14:43 +0700

    Подготовка инфраструктуры WPF-приложения

---
+++ UI/WeatherAcquisition.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MathCore.WPF.ViewModels;
+
+namespace WeatherAcquisition.WPF.ViewModels
+{
+    internal class MainWindowViewModel : TitledViewModel
+    {
+        public MainWindowViewModel()
+        {
+            Title = "Application Main Window";
+        }
+    }
+}
