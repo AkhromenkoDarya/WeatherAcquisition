04011ffa44cab2ca50842d82ae3474cb451395ed
Darya Akhromenko <otulissa2012@yandex.ru>
2022-07-18 16:14:50 +0700

    Подготовка контекста базы данных

---
+++ Data/WeatherAcquisition.DAL/Context/DataDB.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherAcquisition.DAL.Entities;
+
+namespace WeatherAcquisition.DAL.Context
+{
+    public class DataDb : DbContext
+    {
+        public DbSet<DataSource> Sources { get; set; }
+
+        public DbSet<DataValue> Values { get; set; }
+
+        public DataDb(DbContextOptions<DataDb> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DataSource>()
+                .HasMany<DataValue>()
+                .WithOne(v => v.Source)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
