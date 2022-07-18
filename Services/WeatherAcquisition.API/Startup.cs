8d2ca94fe35244ef8db398c3579742b5243c8853
Darya Akhromenko <otulissa2012@yandex.ru>
2022-07-18 16:40:44 +0700

    Первая миграция БД

--- Services/WeatherAcquisition.API/Startup.cs
+++ Services/WeatherAcquisition.API/Startup.cs
@@ -1,47 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using WeatherAcquisition.DAL.Context;
 
 namespace WeatherAcquisition.API
 {
-    public class Startup
+    public record Startup(IConfiguration Configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataDb>(
+                options => options
+                .UseSqlServer(
+                    Configuration.GetConnectionString("Data"), 
+                    opt => opt.MigrationsAssembly("WeatherAcquisition.DAL.SqlServer")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WeatherAcquisition.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WeatherAcquisition.API", 
+                    Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WeatherAcquisition.API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", 
+                    "WeatherAcquisition.API v1"));
             }
 
             app.UseHttpsRedirection();
