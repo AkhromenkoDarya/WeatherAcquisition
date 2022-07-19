9e0cdf6801e7a4f71be05fbb0be549d853dd3f4f
Darya Akhromenko <otulissa2012@yandex.ru>
2022-07-19 09:10:16 +0700

    WebAPI для доступа к репозиторию источников данных

---
+++ Services/WeatherAcquisition.API/Controllers/DataSourcesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeatherAcquisition.DAL.Entities;
+using WeatherAcquisition.Interfaces.Base.Repositories;
+
+namespace WeatherAcquisition.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataSourcesController : ControllerBase
+    {
+        private readonly IRepository<DataSource> _repository;
+
+        public DataSourcesController(IRepository<DataSource> repository) => 
+            _repository = repository;
+
+        [HttpGet("count")]
+        public async Task<IActionResult> GetItemCount() => Ok(await _repository.GetCount());
+    }
+}
