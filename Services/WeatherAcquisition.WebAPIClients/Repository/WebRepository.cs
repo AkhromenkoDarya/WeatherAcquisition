75cdc9b2e4b6e891ab7c75e4cbd9409b10e3f097
Darya Akhromenko <otulissa2012@yandex.ru>
2022-07-20 15:08:31 +0700

    Универсальный клиент для WebAPI-репозитория

--- Services/WeatherAcquisition.WebAPIClients/Repository/WebRepository.cs
+++ Services/WeatherAcquisition.WebAPIClients/Repository/WebRepository.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using WeatherAcquisition.Interfaces.Base.Entities;
@@ -13,59 +16,135 @@ namespace WeatherAcquisition.WebAPIClients.Repository
 
         public WebRepository(HttpClient client) => _client = client;
 
-        public Task<bool> ContainsId(int id, CancellationToken cancellationToken = default)
+        public async Task<int> GetCount(CancellationToken cancellationToken = default) =>
+            await _client.GetFromJsonAsync<int>("count", cancellationToken)
+                .ConfigureAwait(false);
+
+        public async Task<bool> ContainsId(int id, CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
+            HttpResponseMessage response = await _client.GetAsync($"exist/id/{id}", 
+                    cancellationToken).ConfigureAwait(false);
+            return response.StatusCode != HttpStatusCode.NotFound && response.IsSuccessStatusCode;
         }
 
-        public Task<bool> Contains(T item, CancellationToken cancellationToken = default)
+        public async Task<bool> Contains(T item, CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
+            HttpResponseMessage response = await _client.PostAsJsonAsync($"exist", item,
+                    cancellationToken).ConfigureAwait(false);
+            return response.StatusCode != HttpStatusCode.NotFound && response.IsSuccessStatusCode;
         }
 
-        public Task<int> GetCount(CancellationToken cancellationToken = default)
+        public async Task<IEnumerable<T>> GetAll(CancellationToken cancellationToken = default) =>
+            await _client.GetFromJsonAsync<IEnumerable<T>>("", cancellationToken)
+                .ConfigureAwait(false);
+
+        public async Task<IEnumerable<T>> Get(int skip, int count,
+            CancellationToken cancellationToken = default) =>
+            await _client.GetFromJsonAsync<IEnumerable<T>>($"items[{skip}:{count}]", 
+                cancellationToken).ConfigureAwait(false);
+
+        public async Task<IPage<T>> GetPage(int pageIndex, int pageSize, 
+            CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
+            HttpResponseMessage response = await _client.GetAsync($"page[{pageIndex}/{pageSize}]", 
+                cancellationToken).ConfigureAwait(false);
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return new PageItems
+                {
+                    Items = Enumerable.Empty<T>(),
+                    TotalItemCount = 0,
+                    PageIndex = pageIndex,
+                    PageSize = pageSize,
+                    TotalPageCount = 0
+                };
+            }
+
+            return await response
+                .EnsureSuccessStatusCode()
+                .Content
+                .ReadFromJsonAsync<PageItems>(cancellationToken: cancellationToken)
+                .ConfigureAwait(false);
         }
 
-        public Task<IEnumerable<T>> GetAll(CancellationToken cancellationToken = default)
+        private class PageItems : IPage<T>
         {
-            throw new System.NotImplementedException();
+            public IEnumerable<T> Items { get; init; }
+
+            public int TotalItemCount { get; init; }
+
+            public int PageIndex { get; init; }
+
+            public int PageSize { get; init; }
+
+            public int TotalPageCount { get; init; }
         }
 
-        public Task<IEnumerable<T>> Get(int skip, int count, CancellationToken cancellationToken = default)
+        public async Task<T> GetById(int id, CancellationToken cancellationToken = default) =>
+            await _client.GetFromJsonAsync<T>($"{id}", cancellationToken)
+                .ConfigureAwait(false);
+
+        public async Task<T> Add(T item, CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
+            HttpResponseMessage response = await _client.PostAsJsonAsync("", item, 
+                cancellationToken).ConfigureAwait(false);
+            T result = await response
+                .EnsureSuccessStatusCode()
+                .Content
+                .ReadFromJsonAsync<T>(cancellationToken: cancellationToken)
+                .ConfigureAwait(false);
+            return result;
         }
 
-        public Task<IPage<T>> GetPage(int pageIndex, int pageSize, CancellationToken cancellationToken = default)
+        public async Task<T> Update(T item, CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
+            HttpResponseMessage response = await _client.PutAsJsonAsync("", item,
+                cancellationToken).ConfigureAwait(false);
+            T result = await response
+                .EnsureSuccessStatusCode()
+                .Content
+                .ReadFromJsonAsync<T>(cancellationToken: cancellationToken)
+                .ConfigureAwait(false);
+            return result;
         }
 
-        public Task<T> GetById(int id, CancellationToken cancellationToken = default)
+        public async Task<T> Delete(T item, CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
+            //return await DeleteById(item.Id, cancellationToken).ConfigureAwait(false);
+
+            var request = new HttpRequestMessage(HttpMethod.Delete, "")
+            {
+                Content = JsonContent.Create(item)
+            };
+
+            HttpResponseMessage response = await _client.SendAsync(request, cancellationToken)
+                .ConfigureAwait(false);
+
+            T result = await response
+                .EnsureSuccessStatusCode()
+                .Content
+                .ReadFromJsonAsync<T>(cancellationToken: cancellationToken)
+                .ConfigureAwait(false);
+            return result;
         }
 
-        public Task<T> Add(T item, CancellationToken cancellationToken = default)
+        public async Task<T> DeleteById(int id, CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
-        }
+            HttpResponseMessage response = await _client.DeleteAsync($"{id}", 
+                cancellationToken).ConfigureAwait(false);
 
-        public Task<T> Update(T item, CancellationToken cancellationToken = default)
-        {
-            throw new System.NotImplementedException();
-        }
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return default;
+            }
 
-        public Task<T> Delete(T item, CancellationToken cancellationToken = default)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<T> DeleteById(int id, CancellationToken cancellationToken = default)
-        {
-            throw new System.NotImplementedException();
+            T result = await response
+                .EnsureSuccessStatusCode()
+                .Content
+                .ReadFromJsonAsync<T>(cancellationToken: cancellationToken)
+                .ConfigureAwait(false);
+            return result;
         }
     }
 }
