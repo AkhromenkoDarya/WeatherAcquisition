068f14a5bdc5bf87cab8021e136f2515c64e4c5e
Darya Akhromenko <otulissa2012@yandex.ru>
2022-07-23 16:37:26 +0700

    Удаление данных со стороны сервера

--- Common/WeatherAcquisition.Domain.Base/DataSourceInfo.cs
+++ Common/WeatherAcquisition.Domain.Base/DataSourceInfo.cs
@@ -1,13 +1,40 @@
-﻿using WeatherAcquisition.Interfaces.Base.Entities;
+﻿using System;
+using WeatherAcquisition.Interfaces.Base.Entities;
 
 namespace WeatherAcquisition.Domain.Base
 {
-    public class DataSourceInfo : INamedEntity
+    public class DataSourceInfo : INamedEntity, IEquatable<DataSourceInfo>
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
 
         public string Description { get; set; }
+
+        public bool Equals(DataSourceInfo other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id == other.Id && Name == other.Name && Description == other.Description;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DataSourceInfo)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = Id;
+                hashCode = (hashCode * 397) ^ (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Description != null ? Description.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
     }
 }
